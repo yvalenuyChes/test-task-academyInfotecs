0f47c00a6075c866fcf3c25db175b8117855f297
Vladislav Zaporozhets <vladislav.zaporozhets@haiku.dev>
2021-12-24 21:25:32 +0300

    0.4

--- src/js/index.js
+++ src/js/index.js
@@ -2,33 +2,42 @@ import * as data from '../store/DB.json'
 const tableBody = document.querySelector('.table__body')
 
 
-// const appData = JSON.parse(data)
-
-console.log()
-
-
-
-// table.innerHTML += `
-// ${
-//     for (let i = 0; i < data.length; i++){
-
-// }
-// }
-// `
-
+// получаем и выводим всех пользователей из хранилища
 for(let i = 0; i < data.length; i++){
    tableBody.innerHTML += `
-   <li>
-   <h2>Name:${data[i].name.firstName}</h2>
-   <br/>
-   <h2>Last Name:${data[i].name.lastName}</h2>
-   <br/>
-   <p>About:
-   ${data[i].about}
-   </p>
-   <br/>
-   ${data[i].eyeColor}
+   <li class="table__item" >
+      <h2 class="table__item-title" >
+         Name:${data[i].name.firstName}
+      </h2>
+      <br/>
+      <h2 class="table__item-subtitle" >
+         Lastname:${data[i].name.lastName}
+      </h2>
+      <br/>
+      <div class="table__item-description hidden">
+         <p>
+            About: ${data[i].about}
+         </p>
+         <button class="table__item-description-button">
+            Learn more
+         </button>
+      </div>
+
+      <br/>
+      <br/>
+      <p>
+         Eye color: ${data[i].eyeColor}
+      </p>
    </li>
    `
+}
+
+const showTextButtonArray = tableBody.querySelectorAll('.table__item-description-button')
+
+for(let i = 0; i < showTextButtonArray.length; i++){
+   showTextButtonArray[i].addEventListener('click', event=> {
+      const text = event.target.closest('.table__item-description')
+      text.classList.toggle('hidden')
+   })
 }
 
