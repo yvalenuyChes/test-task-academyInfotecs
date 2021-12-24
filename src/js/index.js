dc60db8df917c3d15e80e76d038268192641679d
Vladislav Zaporozhets <vladislav.zaporozhets@haiku.dev>
2021-12-25 01:01:13 +0300

    begin implemente filter for colums

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,23 @@
 import * as data from '../store/DB.json'
 const tableBody = document.querySelector('.table__body')
+const colFilter = document.querySelector('.table__filter')
 
 
-// получаем и выводим всех пользователей из хранилища
+//? получаем и выводим всех пользователей из хранилища
+
 for(let i = 0; i < data.length; i++){
    tableBody.innerHTML += `
    <li class="table__item" >
+   <div>
       <h2 class="table__item-title" >
          Name:${data[i].name.firstName}
       </h2>
-      <br/>
+   </div>
+   <div>
       <h2 class="table__item-subtitle" >
-         Lastname:${data[i].name.lastName}
+         Surname:${data[i].name.lastName}
       </h2>
-      <br/>
+   </div>
       <div class="table__item-description hidden">
          <p>
             About: ${data[i].about}
@@ -22,15 +26,16 @@ for(let i = 0; i < data.length; i++){
             Learn more
          </button>
       </div>
-
-      <br/>
-      <br/>
-      <p>
-         Eye color: ${data[i].eyeColor}
-      </p>
+      <div>
+         <p>
+            Eye color: <span style='color:${data[i].eyeColor}'>${data[i].eyeColor}</span>
+         </p>
+      </div>
    </li>
    `
 }
+
+//? функция скрытия/показа текста
 
 const showTextButtonArray = tableBody.querySelectorAll('.table__item-description-button')
 
@@ -40,4 +45,21 @@ for(let i = 0; i < showTextButtonArray.length; i++){
       text.classList.toggle('hidden')
    })
 }
+
+
+//? функция сортировки колонок
+
+console.log(Object.keys(data[0]).length);
+
+
+
+for(let i = 0; i < (Object.keys(data[0]).length); i++){
+   colFilter.innerHTML += `<option value = ${Object.keys(data[0])[i]}>${Object.keys(data[0])[i]}</option>`
+}
+
+
+
+
+//? функция скрытия/показа колонки
+
 
