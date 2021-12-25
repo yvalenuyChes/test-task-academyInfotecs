2fdfa4d206923c2fb6c3f561bc786c43d8023d73
Vladislav Zaporozhets <vladislav.zaporozhets@haiku.dev>
2021-12-25 15:07:31 +0300

    fix filter makets

--- src/js/filter.js
+++ src/js/filter.js
@@ -60,12 +60,10 @@ export default function filterCards(){
       for(let i = 0; i < data.length; i++){
          tableBody.innerHTML += `
          <li class="table__item" >
-         <div>
-            <h2 class="table__item-title" >
+         <div class="table__item-name">
+            <h2 class="table__item-title">
                Name:${data[i].name.firstName}
             </h2>
-         </div>
-         <div>
             <h2 class="table__item-subtitle" >
                Surname:${data[i].name.lastName}
             </h2>
@@ -78,7 +76,7 @@ export default function filterCards(){
                   Learn more
                </button>
             </div>
-            <div>
+            <div class="eye-color__container">
                <p>
                   Eye color: <span style='color:${data[i].eyeColor}'>${data[i].eyeColor}</span>
                </p>
